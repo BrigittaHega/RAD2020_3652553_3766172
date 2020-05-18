950aaf40001f37e23c2e3db96c2109f221c64eed
Dana Selene <dawanamo@gmail.com>
2020-05-18 15:58:00 +1000

    no error in rails test--Tut 6

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    # binding.pry
+    # debugger
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+      flash[:success] = "Welcome to the Tute 6!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get root_path#--routes.rb--line 6 has been changed into this, Listing 5.28
+    # get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get help" do
+    get help_path#--routes.rb--line below has been changed into this, Listing 5.28
+    # get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+    get about_path#--routes.rb--line below has been changed into this, Listing 5.28
+    # get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contact" do
+    get contact_path#--routes.rb--line below has been changed into this, Listing 5.28
+    # get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    end
+
+end
