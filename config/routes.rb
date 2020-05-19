6fd320b20698efb4abb363aae04e4474ac493ada
Dana Selene <dawanamo@gmail.com>
2020-05-20 03:37:56 +1000

    end of chapter 7-tut 6---sign up function works

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,10 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  # post '/signup', to: 'users#create'
+  post '/signup', to: 'users#create'
 
   get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  # post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
 end
