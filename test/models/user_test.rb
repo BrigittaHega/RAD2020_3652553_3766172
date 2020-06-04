4b5f3e20d0bf2c8d1a5f18c5ea09ef2cceb8e41f
BrigittaHega <akuraegashiro@gmail.com>
2020-06-04 23:10:58 +1000

    before putting in secure password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -47,6 +47,7 @@ class UserTest < ActiveSupport::TestCase
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[rad2020rmit@gmail,com user.name@example.
     			foo@bar_baz.com foo@bar+baz.com]
+
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -60,14 +61,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email addresses should be saved as lower-case" do
+	mixed_case_email = "Foo@ExAMPle.CoM"
+	@user.email = mixed_case_email
+	@user.save
+	assert_equal mixed_case_email.downcase, @user.reload.email
+  end
   # for the phone numbers in user.rb file in app/models:
   # should the phone have the uniqueness? if yes, how should we change the code for email to phone?
-  # example code from email address:
-  # test "email addresses should be unique" do
-  #   duplicate_user = @user.dup
-  #   duplicate_user.email = @user.email.upcase
-  #   @user.save
-  #   assert_not duplicate_user.valid?
-  # end
-  
 end
