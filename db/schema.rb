fcd8d0a15e869135354938a2535f5e8f23cce223
BrigittaHega <akuraegashiro@gmail.com>
2020-06-04 22:58:55 +1000

    indices is in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_103921) do
+ActiveRecord::Schema.define(version: 2020_06_04_125159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_103921) do
     t.integer "mobile"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
