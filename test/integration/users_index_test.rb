dd4909035e63ec1169bd5d10acc00d50d257cbae
BrigittaHega <akuraegashiro@gmail.com>
2020-06-07 19:07:20 +1000

    new branch from commit

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,9 +5,13 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "index" do
+  test "index including pagination" do
   #log_in_as(@user) #Not used because a part of Chap 9
-  get users_path
-  assert_template 'users/index'
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    User.paginate(page: 1).each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    end
   end
 end
